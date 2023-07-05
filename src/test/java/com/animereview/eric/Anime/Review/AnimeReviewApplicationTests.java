65cbfa71f1118b2701f3eb14dd15005c639af5a7
Eric Wang <eqw5149@gmail.com>
2023-07-05 00:10:01 -0400

    First commit

---
+++ src/test/java/com/animereview/eric/Anime/Review/AnimeReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.animereview.eric.Anime.Review;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
